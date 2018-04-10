731a9795a9ac8c25e7d46cd2194d4a8dc04cf212
DingDong <13716785809@163.com>
2018-04-10 17:48:46 +0800

    测试项目

---
+++ src/main/java/com/hollycrm/DesignApplication.java
@@ -0,0 +1,15 @@
+package com.hollycrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 161220a
+ */
+@SpringBootApplication
+public class DesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignApplication.class, args);
+	}
+}
